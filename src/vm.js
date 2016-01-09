63462edad34e33da4191dfde8bfa07842212dede
sheigel <eigel.si@gmail.com>
2016-01-09 18:02:15 +0200

    removed reducers, made start do webpack and server

--- src/vm.js
+++ src/vm.js
@@ -1,20 +1,21 @@
 import ko from 'knockout';
 import 'isomorphic-fetch'
 
-export default function(){
-console.warn("frick")
-  let someText=ko.observable(10),
-    click=()=>{
-        console.log("clicked")
-      fetch('/api')
-        .then(res=>{return res.json()})
-        .then((res)=>{
-        someText(someText()+res+"asdf");
-console.log(res);
-      })};
+export default function () {
+    console.warn("frick")
+    let someText = ko.observable(10),
+        click = ()=> {
+            fetch('/api')
+                .then(res=> {
+                    return res.json()
+                })
+                .then((res)=> {
+                    someText(someText() + res.text);
+                })
+        };
 
-  return {
-    someText,
-    click
-  };
+    return {
+        someText,
+        click
+    };
 }
