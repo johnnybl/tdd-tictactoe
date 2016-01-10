342842434a2e69a48f6f7094258db5ec9a0df5a2
sheigel <eigel.si@gmail.com>
2016-01-10 10:00:40 +0200

    updated window install for manual installation

--- windowsInstall.ps1
+++ windowsInstall.ps1
@@ -1,5 +1,5 @@
 cinst -y nodejs.install
 cinst -y python2
 Invoke-WebRequest https://download.microsoft.com/download/3/8/E/38EE4758-7C31-4D96-8FF9-83CC313F0F78/VisualCppBuildTools_Full.exe -OutFile C:\Users\sheigel\Desktop\vcpp.exe
-C:\Users\sheigel\Desktop\vcpp.exe /Passive /Full
+C:\Users\sheigel\Desktop\vcpp.exe /Passive 
 
