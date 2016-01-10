6f95d36c2e6a5734406d473f3069fe8ab4fd4aa6
john <ionutnegru@gmail.com>
2016-01-10 17:11:51 +0200

    introduced beforeeach in tests

--- test/game.spec.js
+++ test/game.spec.js
@@ -2,40 +2,68 @@ import {expect} from 'chai'
 
 import vm from '../src/vm'
 
-let g = ()=> {
+let gameFactory = ()=> {
     return {
         cells: [null,null,null,null,null,null,null,null,null],
             currentPlayer: "X",
         placeMove:function(location){
         this.cells[location] = this.currentPlayer;
-        this.currentPlayer = "0"
+        this.currentPlayer = this
+            .currentPlayer ==="X"?"0":"X"
     }
     }
 };
 
 describe('newGame', ()=> {
 
+    let game;
+    beforeEach(()=> {
+        game = gameFactory();
+    })
+
     it("has 9 empty cells", () => {
-        expect(g().cells).to.have.length(9);
-        expect(g().cells).to.have.all.equal(null);
+        expect(game.cells).to.have.length(9);
+        expect(game.cells).to.have.all.equal(null);
     })
 
     it("start with X as current player", () => {
-        expect(g().currentPlayer).to.equal("X");
+        expect(gameFactory().currentPlayer).to.equal("X");
     })
 
 })
 
 describe ("first player move", () => {
-    it("places first player token on desired location", () => {
-        let game = g();
+    let game;
+    beforeEach(()=> {
+        game = gameFactory();
         game.placeMove(3);
+    })
+
+    it("changes current player to 0", ()=>{
+        expect(game.currentPlayer).to.equal("0");
+    })
+
+    it("places first player token on desired location", () => {
         expect(game.cells[3]).to.equal("X");
     })
 
-    it("changes current player to Y", ()=>{
-        let game = g();
+
+})
+
+describe("second player move", ()=>{
+
+    let game;
+    beforeEach(()=> {
+        game = gameFactory();
         game.placeMove(3);
-        expect(game.currentPlayer).to.equal("0");
+        game.placeMove(4);
+    })
+
+    it("places second player token on desired location", () => {
+        expect(game.cells[4]).to.equal("0");
+    })
+
+    it("changes current player to X", ()=>{
+        expect(game.currentPlayer).to.equal("X");
     })
 })
