a4d461d53c88429618e6d02452075d73c1c56c04
john <ionutnegru@gmail.com>
2016-01-10 17:38:46 +0200

    cleanup

--- test/game.spec.js
+++ test/game.spec.js
@@ -22,36 +22,36 @@ describe('newGame', ()=> {
     let game;
     beforeEach(()=> {
         game = gameFactory();
-    })
+    });
 
     it("has 9 empty cells", () => {
         expect(game.cells).to.have.length(9);
         expect(game.cells).to.have.all.equal(null);
-    })
+    });
 
     it("start with X as current player", () => {
         expect(gameFactory().currentPlayer).to.equal("X");
     })
 
-})
+});
 
 describe ("first player move", () => {
     let game;
     beforeEach(()=> {
         game = gameFactory();
         game.placeMove(3);
-    })
+    });
 
     it("changes current player to 0", ()=>{
         expect(game.currentPlayer).to.equal("0");
-    })
+    });
 
     it("places first player token on desired location", () => {
         expect(game.cells[3]).to.equal("X");
     })
 
 
-})
+});
 
 describe("second player move", ()=>{
 
@@ -60,16 +60,16 @@ describe("second player move", ()=>{
         game = gameFactory();
         game.placeMove(3);
         game.placeMove(4);
-    })
+    });
 
     it("places second player token on desired location", () => {
         expect(game.cells[4]).to.equal("0");
-    })
+    });
 
     it("changes current player to X", ()=>{
         expect(game.currentPlayer).to.equal("X");
     })
-})
+});
 
 describe("a player wins", ()=>{
     describe("X player wins on first line", ()=>{
@@ -81,7 +81,7 @@ describe("a player wins", ()=>{
             game.placeMove(1);
             game.placeMove(6);
             game.placeMove(2);
-        })
+        });
 
         it("X wins the game", ()=>{
             let output = "";
@@ -90,10 +90,10 @@ describe("a player wins", ()=>{
                     output+="\n";
                 }
                 output+=c;
-            })
+            });
             console.log(output);
             expect(game.winner()).to.equal("X");
         })
 
     })
-})
+});
