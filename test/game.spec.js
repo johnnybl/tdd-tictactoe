6fa80aad8ff72a84dfa68aa829896ee19b24f57a
john <ionutnegru@gmail.com>
2016-01-10 16:09:25 +0200

    refactoring

--- test/game.spec.js
+++ test/game.spec.js
@@ -2,39 +2,40 @@ import {expect} from 'chai'
 
 import vm from '../src/vm'
 
-let g = {
-    cells: [null,null,null,null,null,null,null,null,null],
-    currentPlayer: "X",
-    firstMove:function(location){
-        this.cells[location]="X";
+let g = ()=> {
+    return {
+        cells: [null,null,null,null,null,null,null,null,null],
+            currentPlayer: "X",
+        placeMove:function(location){
+        this.cells[location] = this.currentPlayer;
         this.currentPlayer = "0"
+    }
     }
 };
 
 describe('newGame', ()=> {
 
     it("has 9 empty cells", () => {
-        expect(g.cells).to.have.length(9);
+        expect(g().cells).to.have.length(9);
+        expect(g().cells).to.have.all.equal(null);
     })
 
     it("start with X as current player", () => {
-        expect(g.currentPlayer).to.equal("X");
-    })
-
-    it("has all cells empty", () => {
-        expect(g.cells).to.have.all.equal(null);
+        expect(g().currentPlayer).to.equal("X");
     })
 
 })
 
 describe ("first player move", () => {
     it("places first player token on desired location", () => {
-        g.firstMove(3);
-        expect(g.cells[3]).to.equal("X");
+        let game = g();
+        game.placeMove(3);
+        expect(game.cells[3]).to.equal("X");
     })
 
     it("changes current player to Y", ()=>{
-        g.firstMove(3);
-        expect(g.currentPlayer).to.equal("0");
+        let game = g();
+        game.placeMove(3);
+        expect(game.currentPlayer).to.equal("0");
     })
 })
